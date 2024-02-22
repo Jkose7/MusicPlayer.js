6120baa1a42b702352b9c383da49dc74e836ca5b
Jkose7 <ojosema10@gmail.com>
2024-02-21 23:52:28 -0500

    'song-currently'

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { songs } from './songs.js'
+
+
 const asideMenu = document.querySelector(".aside-menu-player")
 const mainPlayer = document.querySelector(".main-player-container")
 
@@ -6,40 +9,12 @@ document.querySelector('#library').addEventListener('click', () => {
     asideMenu.classList.toggle('aside-hidden')
 })
 
-
-const songs = [
-    {
-        id: '1',
-        artist: 'Radiohead',
-        title: '15 steps',
-        album: 'https://m.media-amazon.com/images/I/A1MwaIeBpwL._UF1000,1000_QL80_.jpg'
-    },
-    {
-        id: '2',
-        artist: 'Deftones',
-        title: 'MX',
-        album: 'https://i.scdn.co/image/ab67616d0000b2730b1129853982ea17845d4eb6'
-    },
-    {
-        id: '3',
-        artist: 'Panchiko',
-        title: 'D>E>A>T>H>M>E>T>A>L',
-        album: 'https://images.genius.com/1a3c3cb9ee5ad4deef217c11610f1a7d.999x999x1.png'
-    },
-    {
-        id: '4',
-        artist: 'Portishead',
-        title: 'Cowboys',
-        album: 'https://upload.wikimedia.org/wikipedia/en/d/dc/Portishead_-_Portishead.png'
-    },
-]
-
 const asideSongContainer = document.querySelector('.aside-menu-player')
 
 let asideSongs = ''
 
 songs.forEach((song) => {
-    const {id, artist, title, album} = song
+    const { id, artist, title, album } = song
 
     asideSongs += `
     <article class="aside-music" id="${id}">
@@ -54,26 +29,66 @@ songs.forEach((song) => {
 
 asideSongContainer.innerHTML = asideSongs
 
+
+
+
+const controllersAudio = () => {
+    let songAudio = document.querySelector('audio')
+    console.log(songAudio)
+    const playButton = document.querySelector('#play')
+    const nextButton = document.querySelector('#next')
+
+    let songState = true
+    playButton.addEventListener('click', () => {
+        if (songState) {
+            songAudio.pause()
+            songState = false
+        } else {
+            songAudio.play()
+            songState = true
+        }
+
+    
+    })
+
+    nextButton.addEventListener('click', () => {
+        const actual = songs.find((songCurrently) => songCurrently.artist === 'Deftones')
+        console.log(songs.indexOf(actual))
+    })
+
+
+}
+
+
 const asideSong = document.querySelectorAll('.aside-music')
-const songCurrently = document.querySelector('.player-music-info')
+const songCurrently = document.querySelector('.song-container')
 
 let songSelected = ''
-asideSong.forEach((songPlay) =>{
-    songPlay.addEventListener('click', ()=>{
+asideSong.forEach((songPlay) => {
+    songPlay.addEventListener('click', () => {
         const idSong = songPlay.id
         const songInfo = songs.find((song) => song.id === idSong)
-        
+
         songSelected = `
-        <img class="player-music-photo"
-        src="${songInfo.album}"
-        alt=""><p>${songInfo.artist}</p>
-        <h1>${songInfo.title}</h1>
-        ` 
-        
+        <article class="player-music-info">
+            <img class="player-music-photo"
+            src="${songInfo.album}"
+            alt="">
+                <p>${songInfo.artist}</p>
+                <h1>${songInfo.title}</h1>
+                <audio autoplay src="${songInfo.mp3}">
+        </audio>
+        </article>
+        `
+
+
         songCurrently.innerHTML = songSelected
+        controllersAudio()
     })
-    
 })
+
+
+
 
 
 
---
+++ songs.js
@@ -0,0 +1,38 @@
+export const songs = [
+    {
+        id: '1',
+        artist: 'Radiohead',
+        title: '15 steps',
+        mp3:'./songs/Deftones-song1.ogg',
+        album: 'https://m.media-amazon.com/images/I/A1MwaIeBpwL._UF1000,1000_QL80_.jpg'
+    },
+    {
+        id: '2',
+        artist: 'Deftones',
+        title: 'Be quiet and drive',
+        mp3:'../songs/Mcr-song1.ogg',
+        album: 'https://i.scdn.co/image/ab67616d0000b2730b1129853982ea17845d4eb6'
+    },
+    {
+        id: '3',
+        artist: 'Panchiko',
+        title: 'D>E>A>T>H>M>E>T>A>L',
+        mp3:'../songs/Mcr-song1.ogg',
+        album: 'https://images.genius.com/1a3c3cb9ee5ad4deef217c11610f1a7d.999x999x1.png'
+    },
+    {
+        id: '4',
+        artist: 'Portishead',
+        title: 'Cowboys',
+        mp3:'./songs/Deftones-song1.ogg',
+        album: 'https://upload.wikimedia.org/wikipedia/en/d/dc/Portishead_-_Portishead.png'
+    },
+    {
+        id: '5',
+        artist: 'Chet Baker',
+        title: 'I fall in love to easily',
+        mp3:'../songs/Mcr-song1.ogg',
+        album: 'https://upload.wikimedia.org/wikipedia/en/6/60/Chet_Baker_Sings.jpg'
+        
+    }
+]
