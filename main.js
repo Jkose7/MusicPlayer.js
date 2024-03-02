d8b64de6370d6fe416fbfdac000e7081c4c22fd2
Jkose7 <ojosema10@gmail.com>
2024-03-02 15:28:02 -0500

    buttons functions

--- main.js
+++ main.js
@@ -1,42 +1,130 @@
 import { songs } from './songs.js'
 
 
-const asideMenu = document.querySelector(".aside-menu-player")
-const mainPlayer = document.querySelector(".main-player-container")
+const showLibrary = () => {
+    const asideMenu = document.querySelector(".aside-menu-player")
+    const mainPlayer = document.querySelector(".main-player-container")
 
-document.querySelector('#library').addEventListener('click', () => {
-    mainPlayer.classList.toggle('aside-hidden-main')
+    mainPlayer.classList.toggle('main-hidden')
     asideMenu.classList.toggle('aside-hidden')
-})
+}
 
-const asideSongContainer = document.querySelector('.aside-menu-player')
+document.querySelector('#library').addEventListener('click', showLibrary)
 
-let asideSongs = ''
 
-songs.forEach((song) => {
-    const { id, artist, title, album } = song
+const addSongsAside = () => {
+    const asideSongContainer = document.querySelector('.aside-menu-player')
 
-    asideSongs += `
-    <article class="aside-music" id="${id}">
-        <img class="aside-music-photo" src="${album}" alt="">
-        <div class="aside-music-info">
-            <strong class="aside-music-title">${title}</strong>
-            <p class="aside-music-artist">${artist}</p>
-        </div>
+    let asideSongs = ''
+
+    songs.forEach((song) => {
+        const { id, artist, title, album } = song
+
+        asideSongs += `
+        <article class="aside-music" id="${id}">
+            <img class="aside-music-photo" src="${album}" alt="">
+            <div class="aside-music-info">
+                <strong class="aside-music-title">${title}</strong>
+                <p class="aside-music-artist">${artist}</p>
+            </div>
+        </article>
+        `
+    })
+
+    asideSongContainer.innerHTML = asideSongs
+}
+
+addSongsAside()
+
+
+const addCurrentSong = (album, artist, title, mp3, id) => {
+    const songCurrently = document.querySelector('.song-container')
+
+    let songCurrentlyContainer = `
+    <article class="player-music-info">
+        <img class="player-music-photo"
+        src="${album}"
+        alt="">
+            <p>${artist}</p>
+            <h1>${title}</h1>
+            <audio src="${mp3}" id=${id}>
+    </audio>
     </article>
     `
+    songCurrently.innerHTML = songCurrentlyContainer
+}
+
+
+document.querySelectorAll('.aside-music').forEach((songSelected) => {
+    songSelected.addEventListener('click', () => {
+        const idArticle = songSelected.id
+        const selectedSong = songs.find((song) => song.id === idArticle)
+
+        const { id, artist, title, album, mp3 } = selectedSong
+
+        addCurrentSong(album, artist, title, mp3, id)
+    })
+
 })
 
-asideSongContainer.innerHTML = asideSongs
+const currentAudio = document.querySelector('audio');
+let currentSongIndex = -1;
+
+const next = () => {
+    if (currentSongIndex === -1) {
+        currentSongIndex = 0;
+    } else {
+        currentSongIndex = (currentSongIndex + 1) % songs.length;
+    }
+
+    const { album, artist, title, mp3, id } = songs[currentSongIndex];
+
+    addCurrentSong(album, artist, title, mp3, id);
+
+    currentAudio.id = id;
+    currentAudio.src = mp3
+
+    console.log({ currentSongIndex });
+};
+
+const prev = () => {
+    
+    if (currentSongIndex === -1 || currentSongIndex === 'undefined') {
+        currentSongIndex = songs.length - 1;
+    } else {
+        currentSongIndex = (currentSongIndex - 1) % songs.length;
+    }
+
+    const { album, artist, title, mp3, id } = songs[currentSongIndex];
+
+    addCurrentSong(album, artist, title, mp3, id);
+
+    currentAudio.id = id;
+    currentAudio.src = mp3
+
+    console.log({ currentSongIndex });
+}
+
+document.querySelector('#next').addEventListener('click', () => next())
+document.querySelector('#prev').addEventListener('click', () => prev())
+document.querySelector('#play').addEventListener('click', () => currentAudio.play())
+document.querySelector('#pause').addEventListener('click', () => currentAudio.pause())
 
 
+
+
+
+
+
+
+/*
 
 
 const controllersAudio = () => {
     let songAudio = document.querySelector('audio')
-    console.log(songAudio)
+    //console.log(songAudio)
     const playButton = document.querySelector('#play')
-    const nextButton = document.querySelector('#next')
+    //const nextButton = document.querySelector('#next')
 
     let songState = true
     playButton.addEventListener('click', () => {
@@ -86,7 +174,7 @@ asideSong.forEach((songPlay) => {
         controllersAudio()
     })
 })
-
+*/
 
 
 
